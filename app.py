81c5afc8e99243ff8f86eba548a70051531da5e4
gkshindia <gkshindia@gmail.com>
2019-07-09 20:09:50 +0530

    JWT, get, put, post, delete methods

--- app.py
+++ app.py
@@ -1,11 +1,64 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key='giktastic'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda n: n['name'] == name, items), None)
+        return {'item' : item}, 200 if item else 400
+
+    def post(self, name):
+        if next(filter(lambda n: n['name'] == name, items), None):
+            return {'message': f"An item with name '{name}' already exist"}, 400
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {"message" : "The item deleted"}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda n: n['name'] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data['price']}
+            items.append(item)
+        item.update(data)
+        return item
+
+
+class ItemList(Resource):
+
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
 if __name__ == '__main__':
