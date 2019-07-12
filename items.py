cc2ca54ca106446675e62e7e936e7211a797b154
gkshindia <gkshindia@gmail.com>
2019-07-12 19:37:23 +0530

    create_table, items, delete, post, put

---
+++ items.py
@@ -0,0 +1,80 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 400
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+        if row:
+            return {"item": {"name": row[0], "price": row[1]}}
+
+
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': f"An item with name '{name}' already exist"}, 400
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        connection =  sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query,(item['name'], item["price"]))
+
+        connection.commit()
+        connection.close()
+
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        if self.find_by_name(name) is None:
+            return {'message': f"The item doesn't exist at all, better check what you are deleting"}, 400
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': f"Item deleted"}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda n: n['name'] == name, items), None)
+        if item is None:
+            item = {"name": name, "price": data['price']}
+            items.append(item)
+        item.update(data)
+        return item
+
+
+class ItemList(Resource):
+
+    def get(self):
+        return {'items': items}
