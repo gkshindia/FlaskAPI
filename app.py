da016f53e260db0901afdc36d03314edd6dc4d53
gkshindia <gkshindia@gmail.com>
2019-07-14 18:56:29 +0530

    delete and create_table

--- app.py
+++ app.py
@@ -12,8 +12,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key='giktastic'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
